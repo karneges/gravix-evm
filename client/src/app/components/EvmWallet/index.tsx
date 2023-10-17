b7f17c5d0a5a61af9021f370afcaf0bb702afdc4
Pavel Ivanov <sudokey@gmail.com>
2023-10-17 17:59:14 +0300

    Remove build info

--- client/src/app/components/EvmWallet/index.tsx
+++ client/src/app/components/EvmWallet/index.tsx
@@ -3,13 +3,14 @@ import { observer } from 'mobx-react-lite'
 import { Button, Typography } from 'antd'
 import { useStore } from "../../hooks/useStore.js";
 import { EvmWalletStore } from "../../stores/EvmWalletStore.js";
+import { sliceAddress } from "../../utils/slice-address.js";
 
 export const EvmWallet: React.FC =  observer(() => {
     const evmWallet = useStore(EvmWalletStore)
 
     return evmWallet.address ? (
         <Typography.Text>
-            {evmWallet.address}
+            {sliceAddress(evmWallet.address)}
         </Typography.Text>
     ) : (
         <Button onClick={evmWallet.connect}>
