a6544e1a7e493b35cf6bc2cfdf4a68fc2d7d031e
matveyb <matvey.b2022@gmail.com>
2023-10-18 12:01:14 +0200

    pos refactoring

--- client/src/app/components/Positions/content.tsx
+++ client/src/app/components/Positions/content.tsx
@@ -1,17 +1,66 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 import styles from './index.module.scss'
 import { useStore } from '../../hooks/useStore.js'
-import { PositionsListStore } from '../../stores/PositionsListStore.js'
+import { PositionsListStore, WithoutArr } from '../../stores/PositionsListStore.js'
 import { observer } from 'mobx-react-lite'
+import { Typography, Table } from 'antd'
+import { formatDate } from '../../utils/format-date.js'
+import { IGravix } from '../../../assets/misc/Gravix.js'
+import { BigNumber } from 'bignumber.js'
+
+const { Title } = Typography
 
 export const PositionsContent: React.FC = observer(() => {
     const positionsList = useStore(PositionsListStore)
 
+    const columns = useMemo(
+        () => [
+            {
+                title: 'Created',
+                dataIndex: '0',
+                key: '0',
+                render: (_: any, item: WithoutArr<IGravix.PositionStructOutput>) => (
+                    <div className={styles.flexCol}>
+                        {[1].map(() => {
+                            console.log(_, 'test')
+                            return 1
+                        })}
+                        <span>
+                            {item.createdAt
+                                ? formatDate(new BigNumber(item.createdAt.toString()).times(1000).toNumber(), 'HH:mm')
+                                : null}
+                        </span>
+                        <span className={styles.date}>
+                            {item.createdAt
+                                ? formatDate(
+                                      new BigNumber(item.createdAt.toString()).times(1000).toNumber(),
+                                      'dd.LL.yyyy',
+                                  )
+                                : null}
+                        </span>
+                    </div>
+                ),
+            },
+            {
+                title: 'Type',
+                dataIndex: 'positionType',
+                key: 'positionType',
+            },
+        ],
+        [],
+    )
+
     return (
         <div className={styles.positions}>
-            {positionsList.allUserPositions.map(_ => JSON.stringify(_))}
-            <span>1</span>
+            <Title level={3}>Positions</Title>
+            <Table
+                dataSource={positionsList.allUserPositions}
+                columns={columns}
+                loading={false}
+                scroll={{ x: true }}
+                rowKey={record => record.createdAt}
+            />
         </div>
     )
 })
